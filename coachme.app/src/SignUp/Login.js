f0139d8b2821653bd2e7e8c7a882b8d0c3828fbf
forexsnyder <forexsnyder@gmail.com>
2020-08-03 07:50:36 -0700

    mon morning

---
+++ coachme.app/src/SignUp/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="App">
+        <div className="poster">
+        <img src="./Poster.jpg"></img>
+        </div>
+        <div className="buttons">
+            <button>Login</button>
+            <button>Sign up</button>
+
+        </div>
+        
+
+    </div>
+  );
+}
+
+export default Login;
